c67dfba87c749c09a79894c1674e9bc34a35df55
Jorge <ogeandes1974@gmail.com>
2024-04-10 18:17:21 -0500

    hecho savingsAccount

--- src/ufps/poo2/ejercicio/banco/SavingsAccount.java
+++ src/ufps/poo2/ejercicio/banco/SavingsAccount.java
@@ -1,25 +1,49 @@
 package ufps.poo2.ejercicio.banco;
 
+/**
+ * Una cuenta de ahorros con interés
+ * @author Jorge Andrés Reyes - jorgeandresrs@ufps.edu.co
+ */
+
 public class SavingsAccount extends Account{
     
     private double interest;
 
-    public SavingsAccount(int accountNumber){
+    /**
+     * Construye una cuenta de ahorros con el número de cuenta y el interés especificados
+     * @param accountNumber el número de la cuenta
+     * @param interesBasico el interés de la cuenta
+     */
+    public SavingsAccount(int accountNumber, double interesBasico){
         super(accountNumber);
+
+        if(interesBasico < 0 || interesBasico > 0.3)
+            throw new RuntimeException("El interés debe ser un valor entre 0 y 0.3");
+
+        this.interest = interesBasico;
     }
 
-    public void addInterest(){
-
+    /**
+     * Construye una cuenta de ahorros con el número de cuenta especificado y con interés de 0
+     * @param accountNumber el número de la cuenta
+     */
+    public SavingsAccount(int accountNumber){
+        this(accountNumber, 0);
     }
 
+    /**
+     * Deposita una cantidad de dinero en la cuenta y le suma el interés
+     * @param sum el monto a depositar
+     */
     @Override
     public void deposit(double sum){
-
+        double interes = super.getBalance() * this.interest;
+        super.deposit(sum + interes);
     }
 
     @Override
     public String toString(){
-        return null;
+        return "Savings " + super.toString();
     }
 
 }
